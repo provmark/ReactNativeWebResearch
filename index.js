b4440868a883c33f6f94b5c4ae052b94c9acd486
provmark <mark.seabourne@providence.org>
2017-10-23 23:41:40 -0700

    Add friends list to mobile

--- index.js
+++ index.js
@@ -1,4 +1,36 @@
-import { AppRegistry } from 'react-native';
-import App from './App';
+import FriendsList from './FriendsList';
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  Text,
+  View
+} from 'react-native';
 
-AppRegistry.registerComponent('ReactNativeWeb', () => App);
+const friends = [
+  {
+    id: 1,
+    firstName: 'Jane',
+    lastName: 'Miller',
+    avatarUrl: 'https://placehold.it/100x100',
+  },
+  {
+    id: 2,
+    firstName: 'Kate',
+    lastName: 'Smith',
+    avatarUrl: 'https://placehold.it/100x100',
+  },
+  {
+    id: 3,
+    firstName: 'Kevin',
+    lastName: 'Yang',
+    avatarUrl: 'https://placehold.it/100x100',
+  },
+];
+
+class ReactNativeWeb extends Component {
+  render() {
+    return <FriendsList friends={friends} />;
+  }
+}
+
+AppRegistry.registerComponent('ReactNativeWeb', () => ReactNativeWeb);
--- readme.md
+++ readme.md
@@ -1,3 +1,6 @@
 Tutorial
 
 https://www.smashingmagazine.com/2016/08/a-glimpse-into-the-future-with-react-native-for-web/
+
+Run Web
+http://localhost:8080/
